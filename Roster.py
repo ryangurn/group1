3456895785d10ffe3f96daf9c220296d280b2fa2
coreekymon <50118585+coreekymon@users.noreply.github.com>
2020-01-25 15:46:16 -0800

    Roster updated

--- Roster.py
+++ Roster.py
@@ -3,53 +3,50 @@
 
 ##Imports
 import csv
+import os.path
 
 ## **Can this automatically detect which format is being uploaded during the read?** 
 # Change between ',' and '\t' for csv or tsv
 delim = '\t'
 
 ## **PEP8 standards, capital Class, rest lowercase**
-class Student:
-    """Class docstring. 
-	Purpose and Behavior: Class used to represent a student. 
-	Class Attriubes: 
-		first : str    first name
-                last : str     last name
-                id : str       student ID
-                email : str    student email
-                phonetic : str phonetic of name
-                reveal : bool  ?? 
 
-	Public Methods:
-        getters and setters needed? 
-      """
+class Student:
 
 	def __init__(self, first, last, ID, email, phonetic, reveal):
-	    """ Parameters: See Student class for descriptions """
-
 		self.first = first
 		self.last = last
 		self.ID = ID
 		self.email = email
 		self.phonetic = phonetic
 		self.reveal = reveal
-        
-	## **Need __str__ or __repr__**
+		
+	def __repr__(self):
+		return 'student named {self.first} {self.last} ID {self.ID}'.format(self=self)
 		
 		
 #list of Student objects
-list = []  ##** should this be it's own class, "Roster" ?** 
+class Roster:
+	def __init__(self):
+		self.students = []
+		
+	def import_roster(self, filename):
+		self.students.clear()
+		if os.path.exists(filename):
+			with open(filename) as csv_file:
+				csv_reader = csv.reader(csv_file, delimiter= delim)
+				for row in csv_reader: ##each row is a student's information
+					self.students.append(Student(row[0], row[1], row[2], row[3], row[4], row[5]))
+				csv_file.close()
+		else:
+			print('File does not exist!')
+			
+	def export_roster(self):
+		expfile = open('export_roster.csv', 'w')
+		for student in self.students:
+			expfile.write('{student.first}{c}{student.last}{c}{student.ID}{c}{student.email}{c}{student.phonetic}{c}{student.reveal}\n'.format(student=student, c=delim))
+		expfile.close()	
 
+	def __str__(self):
+		return '{self.students}'.format(self=self)
 
-#get roster file  ##**this should be a function of Roster class. "read_roster()" or something**
-filename = raw_input("Enter roster file:")
-
-#fill list with students from roster file  ##** This should be a function of Roster class, "populate()" or something.
-with open(filename) as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter= delim)
-	for row in csv_reader:  ##each row is a student's information
-		list.append(Student(row[0], row[1], row[2], row[3], row[4], row[5]))
-
-#display contents of list  ##**Should be the str or repr of the Roster class**
-for obj in list:
-	print(obj.first, obj.last, obj.ID, obj.email, obj.phonetic, obj.reveal)
