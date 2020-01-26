e4ce19f17b4d549333b7b111d4d04db98696c8d6
Ryan Gurnick <ryangurnick@gmail.com>
2020-01-25 21:01:04 -0800

    Update Roster.py

--- Roster.py
+++ Roster.py
@@ -10,10 +10,25 @@ import os.path
 delim = '\t'
 
 ## **PEP8 standards, capital Class, rest lowercase**
-
 class Student:
+	"""
+	Student Class
+	functions:
+	__init__ -- initialize the student class
+	__repr__ -- representation of the student class
+	"""
 
 	def __init__(self, first, last, ID, email, phonetic, reveal):
+		"""
+		Initialize the student class and data structure
+		Args:
+		first -- first name of the student (first:string)
+		last -- last name of the student (last:string)
+		ID -- UO ID for the student (ID:int<9>)
+		email -- email for the student (email:string)
+		phonetic -- phonetic spelling for the students name (phonetic:string)
+		reveal -- reveal the students phonetic spelling (reveal:bool)
+		"""
 		self.first = first
 		self.last = last
 		self.ID = ID
@@ -22,8 +37,10 @@ class Student:
 		self.reveal = reveal
 		
 	def __repr__(self):
+		"""
+		Defines the representation for the student when converted to string
+		"""
 		return 'student named {self.first} {self.last} ID {self.ID}'.format(self=self)
-		
 		
 #list of Student objects
 class Roster:
