dda7bc485026902211df4a8f51e0ebe1eb74f766
Alex Archer <56787015+ArcherPewPewUO@users.noreply.github.com>
2020-02-03 20:18:16 -0800

    Update start.sh
    updating artifact info

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
+#start.sh is simpel shell script to run the program
+
+#Reviewed 2/3/20 AA
+
+#Authors:
+#(Group 1)
+#Alex Archer
+#Naser Alkhateri
+#Ryan Gurnick
+#Cory Ingram
+#Sean Wilson
+
 #!/bin/bash
 sips -z 200 200 ./images/*.png
 python3 main.py
